b229c03194011d604f80f6f91c4f3057afef1d75
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-06-06 16:37:51 -0600

    initial

---
+++ src/main/java/com/mycompany/examen0611/Examen0611.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen0611;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Examen0611 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
