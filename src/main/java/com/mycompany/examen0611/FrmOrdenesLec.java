56ca26c48b63913df24d4d1e07e62c116f0705bb
oswaldo chilin <oswaldochilin49@gmail.com>
2024-06-10 16:22:02 -0600

    pequeños cambios

--- src/main/java/com/mycompany/examen0611/FrmOrdenesLec.java
+++ src/main/java/com/mycompany/examen0611/FrmOrdenesLec.java
@@ -4,7 +4,14 @@
  */
 package com.mycompany.examen0611;
 
+import Accesodatos.OrdenesDAL;
 import entidades.Ordenes;
+import entidades.Servicios;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import utilerias.OpcionesCRUD;
 
 /**
@@ -12,7 +19,9 @@ import utilerias.OpcionesCRUD;
  * @author R5 8500G
  */
 public class FrmOrdenesLec extends javax.swing.JFrame {
- private OpcionesCRUD OpcionCRUD;
+
+    private OpcionesCRUD OpcionCRUD;
+
     /**
      * Creates new form FrmOrdenesLec
      */
@@ -29,7 +38,7 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        txtNombre = new javax.swing.JTextField();
+        txtMonto = new javax.swing.JTextField();
         jBTNBuscar = new javax.swing.JButton();
         jBTNCrear = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -40,13 +49,18 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        txtNombre.addActionListener(new java.awt.event.ActionListener() {
+        txtMonto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtNombreActionPerformed(evt);
+                txtMontoActionPerformed(evt);
             }
         });
 
         jBTNBuscar.setText("Buscar");
+        jBTNBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBTNBuscarActionPerformed(evt);
+            }
+        });
 
         jBTNCrear.setText("Crear");
         jBTNCrear.addActionListener(new java.awt.event.ActionListener() {
@@ -69,10 +83,25 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTablaOrdenes);
 
         jBTNEditar.setText("Editar");
+        jBTNEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBTNEditarActionPerformed(evt);
+            }
+        });
 
         jBTNEliminar.setText("Eliminar");
+        jBTNEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBTNEliminarActionPerformed(evt);
+            }
+        });
 
         jBTNCancelar.setText("Cancelar");
+        jBTNCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBTNCancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,7 +111,7 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(35, 35, 35)
-                        .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(txtMonto, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jBTNBuscar)
                         .addGap(28, 28, 28)
@@ -104,7 +133,7 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtMonto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBTNBuscar)
                     .addComponent(jBTNCrear))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -120,17 +149,91 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
+    private void txtMontoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMontoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_txtNombreActionPerformed
+    }//GEN-LAST:event_txtMontoActionPerformed
 
     private void jBTNCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBTNCrearActionPerformed
-  // TODO add your handling code here:
+        // TODO add your handling code here:
         OpcionCRUD = OpcionesCRUD.CREAR;
         FrmOrdenesEsc frmProductosEsc = new FrmOrdenesEsc(OpcionCRUD, new Ordenes());
-        frmProductosEsc.setTitle("Crear producto");
+        frmProductosEsc.setTitle("Crear Orden");
         frmProductosEsc.setVisible(true);        // TODO add your handling code here:
     }//GEN-LAST:event_jBTNCrearActionPerformed
+
+    private void jBTNCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBTNCancelarActionPerformed
+        // TODO add your handling code here:
+           this.setVisible(false);
+    }//GEN-LAST:event_jBTNCancelarActionPerformed
+
+    private void jBTNEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBTNEditarActionPerformed
+        // TODO add your handling code here: 
+        int row = jTablaOrdenes.getSelectedRow();
+        if (row != -1) {
+            OpcionCRUD = OpcionesCRUD.MODIFICAR;
+            FrmOrdenesEsc frmOrdenesEsc = new FrmOrdenesEsc(OpcionCRUD, obtenerDatos());
+            frmOrdenesEsc.setTitle("Modificar Orden");
+            frmOrdenesEsc.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(this,
+                    "Seleccionar una fila", "Orden",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_jBTNEditarActionPerformed
+
+    private void jBTNEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBTNEliminarActionPerformed
+        // TODO add your handling code here:
+           int row = jTablaOrdenes.getSelectedRow();
+        if (row != -1) {
+            OpcionCRUD = OpcionesCRUD.ELIMINAR;
+            FrmOrdenesEsc frmProductosEsc = new FrmOrdenesEsc(OpcionCRUD, obtenerDatos());
+            frmProductosEsc.setTitle("Eliminar Orden");
+            frmProductosEsc.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(this,
+                    "Seleccionar una fila", "Orden",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_jBTNEliminarActionPerformed
+
+    private void jBTNBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBTNBuscarActionPerformed
+        // TODO add your handling code here:
+           Ordenes orden = new Ordenes();
+        orden.setMonto(Double.parseDouble(txtMonto.getText()));
+        ArrayList<Ordenes> ordens = OrdenesDAL.buscar(orden);
+        String[] columnas = {"ID Orden", "Fecha", "Monto","ServicioID", "Servicio"};
+        Object[][] datos = new Object[ordens.size()][6];
+        for (int i = 0; i < ordens.size(); i++) {
+            Producto item = ordens.get(i);
+            datos[i][0] = item.getOrderID();
+            datos[i][1] = item.getFecha();
+            datos[i][2] = item.getMonto();
+            datos[i][3] = item.getServiciosID();
+             datos[i][4] = item.getServiciosID().getNombre();
+        }
+        DefaultTableModel modelTable = new DefaultTableModel(datos, columnas);
+        jTablaOrdenes.setModel(modelTable);
+    }//GEN-LAST:event_jBTNBuscarActionPerformed
+
+    private Ordenes obtenerDatos() {
+        Ordenes orden = new Ordenes();
+        int row = jTablaOrdenes.getSelectedRow();
+        orden.setOrderID((int) jTablaOrdenes.getValueAt(row, 0));
+
+        String fechaString = jTablaOrdenes.getValueAt(row, 1).toString();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fecha = LocalDate.parse(fechaString, formatter);
+        orden.setFecha(fecha);
+        orden.setMonto((double) jTablaOrdenes.getValueAt(row, 3));
+        orden.setServiciosID((int) jTablaOrdenes.getValueAt(row, 4));
+
+        Servicios servicio = new Servicios();
+        servicio.setNombre(jTablaOrdenes.getValueAt(row, 5).toString());
+        servicio.setServiciosID((int) jTablaOrdenes.getValueAt(row, 4));
+        orden.setServicios(servicio);
+        return orden;
+    }
 
     /**
      * @param args the command line arguments
@@ -175,6 +278,6 @@ public class FrmOrdenesLec extends javax.swing.JFrame {
     private javax.swing.JButton jBTNEliminar;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTablaOrdenes;
-    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTextField txtMonto;
     // End of variables declaration//GEN-END:variables
 }
